29b7567e62beb8564d9edd5c308d41ee514f2886
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-07-04 16:33:20 +0200

    feat: Finalise general styling
    All components are created with correct styles and sizes. Still dummy content

--- src/components/Dice/Dice.jsx
+++ src/components/Dice/Dice.jsx
@@ -1,3 +1,15 @@
 export default function Dice() {
-  return <div>Dice</div>;
+  return (
+    <div className="dice">
+      <ul className="dice_list">
+        <li className="dice_list_die green">1</li>
+        <li className="dice_list_die">2</li>
+        <li className="dice_list_die green">3</li>
+        <li className="dice_list_die">4</li>
+        <li className="dice_list_die">5</li>
+        <li className="dice_list_die">6</li>
+      </ul>
+      <button>Roll</button>
+    </div>
+  );
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,14 @@
+import Dice from "../../components/Dice/Dice";
+
 export default function App() {
   return (
-    <main>
+    <main className="main">
       <h1 className="title">Tenzies</h1>
       <p>
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+      <Dice />
     </main>
   );
 }
